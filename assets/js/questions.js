e8daf216023406cf419d3a2aa44014cd7e47eadf
Nardacyon <63487094+Nardacyon@users.noreply.github.com>
2020-07-27 17:44:55 -0700

    added an array filled with questions, choices, and answers

---
+++ assets/js/questions.js
@@ -0,0 +1,27 @@
+const questions = [
+  {
+    title: "Commonly used data types DO NOT include _________.",
+    choices: ["strings","booleans","alerts","numbers"],
+    answer: "alerts"
+  },
+  {
+    title: "The condition in an if / else statement is enclosed within ____.",
+    choices: ["quotes","curly braces","parentheses","square brackets"],
+    answer: "parentheses"
+  },
+  {
+    title: "Arrays in JavaScript can be used to store _____",
+    choices: ["numbers and strings","other arrays","booleans","all of the above"],
+    answer: "all of the above"
+  },
+  {
+    title: "c",
+    choices: ["Choice 1","Choice 2","Choice 3","Choice 4"],
+    answer: ""
+  },  
+  {
+    title: "d",
+    choices: ["Choice 1","Choice 2","Choice 3","Choice 4"],
+    answer: ""
+  }
+];
