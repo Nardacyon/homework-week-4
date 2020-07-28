5121701c208e13c55489ea542a638d09ead3d1eb
Nardacyon <63487094+Nardacyon@users.noreply.github.com>
2020-07-27 17:46:30 -0700

    added references to DOM elements and created functions for the general purpose of the app

---
+++ assets/js/logic.js
@@ -0,0 +1,35 @@
+var currentQuestionIndex = 0;
+var timer;
+var secondsTime = questions.length * 10;
+
+var questionEl = document.getElementById("questions");
+var timerEl = document.getElementById("timer");
+var choice = document.getElementById("choice");
+var submitEl = document.getElementById("submit");
+var startEl = document.getElementById("start");
+var initialsEl = document.getElementById("initials");
+
+
+startEl.addEventListener('click', startQuiz);
+
+//hides the elements from the main page
+$("#choice").hide();
+$(".end-screen").hide();
+
+function startQuiz() {
+  $(".start-screen").hide();
+  
+
+}
+
+function getQuestion() {
+
+}
+
+function endQuiz() {
+
+}
+
+function setTime() {
+
+}
