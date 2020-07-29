6a3749edd1a13c26445682931e95be1b390333f7
Nardacyon <63487094+Nardacyon@users.noreply.github.com>
2020-07-28 17:40:23 -0700

    added more questions to the array

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,27 +1,27 @@
 const questions = [
   {
-    title: "Commonly used data types DO NOT include _________.",
-    choices: ["strings","booleans","alerts","numbers"],
+    title: "Commonly used data types DO NOT include :",
+    choices: ["strings","booleans", "alerts", "numbers"],
     answer: "alerts"
   },
   {
     title: "The condition in an if / else statement is enclosed within ____.",
-    choices: ["quotes","curly braces","parentheses","square brackets"],
+    choices: ["quotes", "curly braces", "parentheses", "square brackets"],
     answer: "parentheses"
   },
   {
     title: "Arrays in JavaScript can be used to store _____",
-    choices: ["numbers and strings","other arrays","booleans","all of the above"],
+    choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
     answer: "all of the above"
   },
   {
-    title: "c",
-    choices: ["Choice 1","Choice 2","Choice 3","Choice 4"],
-    answer: ""
+    title: "String values must be enclosed within ______ when being assigned to variables",
+    choices: ["commas", "curly braces", "quotes", "parentheses"],
+    answer: "quotes"
   },  
   {
-    title: "d",
-    choices: ["Choice 1","Choice 2","Choice 3","Choice 4"],
-    answer: ""
+    title: "A very useful tool used during development and debugging for printing content to the debugger is _______",
+    choices: ["Javascript", "terminal/bash", "for loops", "console.log"],
+    answer: "console.log"
   }
 ];
